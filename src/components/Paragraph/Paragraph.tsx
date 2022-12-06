2f2b8dbc0c7fc5a025054044ceaabbe3c060c17f
Nathanael Björkgård <nathanael@bjorkgard.se>
2022-12-06 18:03:28 +0100

    Update paragraph component to include multiple elements

--- src/components/Paragraph/Paragraph.tsx
+++ src/components/Paragraph/Paragraph.tsx
@@ -1,8 +1,8 @@
 import React, { FC } from 'react'
 import { ParagraphProps } from './Paragraph.types'
 
-const Paragraph: FC<ParagraphProps> = ({ text }) => {
-  return <p className='mb-2 font-roboto text-sm font-medium'>{text}</p>
-}
+const Paragraph: FC<ParagraphProps> = ({ children }) => (
+  <div className='mb-2 space-y-2 font-roboto text-sm font-medium'>{children}</div>
+)
 
 export default Paragraph
--- src/stories/Paragraph.stories.tsx
+++ src/stories/Paragraph.stories.tsx
@@ -11,5 +11,35 @@ const Template: ComponentStory<typeof Paragraph> = (args) => <Paragraph {...args
 
 export const Default = Template.bind({})
 Default.args = {
-  text: 'Deserunt esse qui aute ipsum quis eu enim. Occaecat cillum nulla fugiat amet eiusmod deserunt nisi velit commodo cillum quis. In magna non id sit labore irure ea laboris fugiat mollit pariatur est nisi. Ullamco enim dolore ex minim proident fugiat eu est fugiat ipsum. Culpa anim aliquip anim ad exercitation deserunt fugiat velit incididunt minim. Eiusmod fugiat eiusmod ea dolore laborum veniam amet consequat. Velit do enim quis esse ad laboris sunt sunt.',
+  children: (
+    <p>
+      Consequat aliquip tempor proident culpa adipisicing laboris enim adipisicing. Elit enim
+      proident eu duis irure consequat mollit eu consectetur labore. Id nulla voluptate velit tempor
+      aute Lorem ullamco minim esse. Veniam laboris sit nisi id qui officia incididunt sit. Et
+      mollit proident ullamco deserunt amet proident labore nostrud. Incididunt irure dolore
+      deserunt do labore mollit fugiat nulla exercitation non sit in nostrud ad.
+    </p>
+  ),
+}
+
+export const MultipleParagraphs = Template.bind({})
+MultipleParagraphs.args = {
+  children: (
+    <>
+      <p>
+        Consequat aliquip tempor proident culpa adipisicing laboris enim adipisicing. Elit enim
+        proident eu duis irure consequat mollit eu consectetur labore. Id nulla voluptate velit
+        tempor aute Lorem ullamco minim esse. Veniam laboris sit nisi id qui officia incididunt sit.
+        Et mollit proident ullamco deserunt amet proident labore nostrud. Incididunt irure dolore
+        deserunt do labore mollit fugiat nulla exercitation non sit in nostrud ad.
+      </p>
+      <p>
+        Consequat aliquip tempor proident culpa adipisicing laboris enim adipisicing. Elit enim
+        proident eu duis irure consequat mollit eu consectetur labore. Id nulla voluptate velit
+        tempor aute Lorem ullamco minim esse. Veniam laboris sit nisi id qui officia incididunt sit.
+        Et mollit proident ullamco deserunt amet proident labore nostrud. Incididunt irure dolore
+        deserunt do labore mollit fugiat nulla exercitation non sit in nostrud ad.
+      </p>
+    </>
+  ),
 }
