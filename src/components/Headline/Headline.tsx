a92c7cb85d6da61ffab37aab6e34e4396ed699b3
Nathanael Björkgård <nathanael@bjorkgard.se>
2022-12-06 15:46:11 +0100

    Fix: Typo on class name

--- src/components/Headline/Headline.tsx
+++ src/components/Headline/Headline.tsx
@@ -2,7 +2,7 @@ import React, { FC } from 'react'
 import { HeadlineProps } from './Headline.types'
 
 const Headline: FC<HeadlineProps> = ({ label }) => {
-  return <h3 className='font-roboto text-2xl leading-6 text-black'>{label}</h3>
+  return <h3 className='font-roboto text-2xl font-black leading-6'>{label}</h3>
 }
 
 export default Headline
