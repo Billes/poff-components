22c616568cdb6304e376fe4744c8e50456c03ce0
Nathanael Björkgård <nathanael@bjorkgard.se>
2022-12-09 16:42:20 +0100

    fix: Add missing props

--- src/components/Headline/Headline.tsx
+++ src/components/Headline/Headline.tsx
@@ -1,8 +1,12 @@
 import React, { FC } from 'react'
 import { HeadlineProps } from './Headline.types'
 
-const Headline: FC<HeadlineProps> = ({ label }) => {
-  return <h3 className='font-roboto text-2xl font-black leading-6 text-black'>{label}</h3>
+const Headline: FC<HeadlineProps> = ({ label, ...props }) => {
+  return (
+    <h3 className='font-roboto text-2xl font-black leading-6 text-black' {...props}>
+      {label}
+    </h3>
+  )
 }
 
 export default Headline
