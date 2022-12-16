2dd45dd6732d94f8004be1d5fb496fc5e51d8263
Nathanael Björkgård <nathanael@bjorkgard.se>
2022-12-16 13:04:00 +0100

    docs: Stories for icon button

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,40 +7,53 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 
-## 1.2.3 - 2022-12-15
+### Added
 
-## 1.2.2 - 2022-12-15
+- Icon button
 
 ## 1.2.1 - 2022-12-15
+
 ### Fixed
+
 - Refactor Alert
 
 ## 1.2.0 - 2022-12-15
+
 ### Added
+
 - Alert component
 - Storybook for Alert in 8 different states
 
 ### Fixed
+
 - Wrong class names on Toggle
 
 ## 1.1.9 - 2022-12-14
 
 ## 1.1.8 - 2022-12-14
+
 ### Fixed
+
 - Add peerDependencies for @tailwindcss/forms
 - Add name to Input-component
 - Add name to Textarea-component
 
 ## 1.1.7 - 2022-12-14
+
 ### Fixed
+
 - Updated classes on Input-component
 - Updated classes on Textarea-component
 
 ## 1.1.6 - 2022-12-14
+
 ### Fixed
+
 - Export InputType
 
 ## 1.1.3 - 2022-12-14
+
 ### Added
+
 - Changelog
 - An icon props on Input-component
---
+++ src/stories/IconButton.stories.tsx
@@ -0,0 +1,54 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react'
+import { MagnifyingGlassIcon, PlusIcon, MinusIcon } from '@heroicons/react/24/solid'
+import IconButton from '../components/IconButton'
+import { IconButtonType } from '../components/IconButton/IconButton.types'
+
+export default {
+  title: 'Form/IconButton',
+  component: IconButton,
+  argTypes: {
+    onClick: { action: 'Button click' },
+    disabled: {
+      control: {
+        type: 'boolean',
+      },
+    },
+  },
+} as ComponentMeta<typeof IconButton>
+
+const Template: ComponentStory<typeof IconButton> = (args) => <IconButton {...args} />
+
+export const Primary = Template.bind({})
+Primary.args = {
+  type: IconButtonType.PRIMARY,
+  icon: <PlusIcon className='h-6 w-6' aria-hidden='true' />,
+  disabled: false,
+}
+
+export const Danger = Template.bind({})
+Danger.args = {
+  type: IconButtonType.DANGER,
+  icon: <MinusIcon className='h-6 w-6' aria-hidden='true' />,
+  disabled: false,
+}
+
+export const Accent = Template.bind({})
+Accent.args = {
+  type: IconButtonType.ACCENT,
+  icon: <MagnifyingGlassIcon className='h-5 w-5' aria-hidden='true' />,
+  disabled: false,
+}
+
+export const Default = Template.bind({})
+Default.args = {
+  type: IconButtonType.DEFAULT,
+  icon: <MagnifyingGlassIcon className='h-5 w-5' aria-hidden='true' />,
+  disabled: false,
+}
+
+export const Disabled = Template.bind({})
+Disabled.args = {
+  type: IconButtonType.PRIMARY,
+  icon: <MagnifyingGlassIcon className='h-5 w-5' aria-hidden='true' />,
+  disabled: true,
+}
