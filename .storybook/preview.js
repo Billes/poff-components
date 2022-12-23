07161bfb7875fbc382d93fb9ea440a83aabaeb6b
Nathanael Björkgård <nathanael@bjorkgard.se>
2022-12-23 08:44:49 +0100

    Update themes on storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -3,6 +3,13 @@ import '!style-loader!css-loader!postcss-loader!tailwindcss/tailwind.css'
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  backgrounds: {
+    default: 'poff',
+    values: [
+      { name: 'poff', value: '#e0f2fe' },
+      { name: 'light', value: '#ffffff' },
+    ],
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
