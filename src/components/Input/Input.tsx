1eb03a4afb0a8e499251f21ecabbb74d26dac127
Nathanael Björkgård <nathanael@bjorkgard.se>
2022-12-23 11:23:19 +0100

    Error messages

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,4 @@
-import React, { FC } from 'react'
+import { FC, FormEvent, useState } from 'react'
 import { InputProps, InputType } from './Input.types'
 import classNames from '../../utils/joinClassNames'
 
@@ -15,39 +15,60 @@ const Input: FC<InputProps> = ({
   value,
   ref,
   ...props
-}) => (
-  <div className='relative rounded-md shadow-sm'>
-    {icon && (
-      <div className='pointer-events-none absolute inset-y-0 left-0 flex items-center pl-2'>
-        {icon}
-      </div>
-    )}
+}) => {
+  const [validationMessage, setValidationMessage] = useState<string>('')
 
-    <input
-      id={id}
-      ref={ref}
-      className={classNames(
-        'block w-full rounded-md',
-        'border border-gray-300',
-        'font-roboto font-light text-gray-900 sm:text-sm',
-        'focus:outline-none',
-        'disabled:cursor-not-allowed disabled:opacity-75',
-        icon ? 'pl-9' : '',
-        error
-          ? 'border-red-700 text-red-700 focus:ring-1 focus:ring-red-700'
-          : 'focus:border-sky-600 focus:ring-1 focus:ring-sky-600',
+  const onInvalid = (e: FormEvent) => {
+    const target = e.target as HTMLInputElement
+    setValidationMessage(target.validationMessage)
+  }
+
+  const onBlur = (e: FormEvent) => {
+    const target = e.target as HTMLInputElement
+
+    // eslint-disable-next-line no-extra-boolean-cast
+    if (!!validationMessage) {
+      setValidationMessage(target.validationMessage)
+    }
+  }
+
+  return (
+    <div className='relative rounded-md shadow-sm'>
+      {icon && (
+        <div className='pointer-events-none absolute inset-y-0 left-0 flex items-center pl-2'>
+          {icon}
+        </div>
       )}
-      type={type}
-      disabled={disabled}
-      required={required}
-      placeholder={placeholder}
-      name={name}
-      value={value}
-      onChange={onChange}
-      {...props}
-    />
-    {error && <span className='ml-2 text-xs text-red-500'>{error}</span>}
-  </div>
-)
+
+      <input
+        id={id}
+        ref={ref}
+        className={classNames(
+          'block w-full rounded-md',
+          'border border-gray-300',
+          'font-roboto font-light text-gray-900 sm:text-sm',
+          'focus:outline-none',
+          'disabled:cursor-not-allowed disabled:opacity-75',
+          'invalid:border-red-700 invalid:text-red-700 invalid:focus:ring-1 invalid:focus:ring-red-700',
+          'focus:border-sky-600 focus:ring-1 focus:ring-sky-600',
+          icon ? 'pl-9' : '',
+        )}
+        type={type}
+        disabled={disabled}
+        required={required}
+        placeholder={placeholder}
+        name={name}
+        value={value}
+        onBlur={onBlur}
+        onChange={onChange}
+        onInvalid={onInvalid}
+        {...props}
+      />
+      {!!validationMessage && (
+        <span className='ml-2 text-xs text-red-500'>{error || validationMessage}</span>
+      )}
+    </div>
+  )
+}
 
 export default Input
--- src/components/Textarea/Textarea.tsx
+++ src/components/Textarea/Textarea.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import { FC, FormEvent, useState } from 'react'
 import { TextareaProps } from './Textarea.types'
 import classNames from '../../utils/joinClassNames'
 
-const Textarea: React.FC<TextareaProps> = ({
+const Textarea: FC<TextareaProps> = ({
   id,
   disabled = false,
   required = false,
@@ -13,6 +13,22 @@ const Textarea: React.FC<TextareaProps> = ({
   onChange,
   ...props
 }) => {
+  const [validationMessage, setValidationMessage] = useState<string>('')
+
+  const onInvalid = (e: FormEvent) => {
+    const target = e.target as HTMLInputElement
+    setValidationMessage(target.validationMessage)
+  }
+
+  const onBlur = (e: FormEvent) => {
+    const target = e.target as HTMLInputElement
+
+    // eslint-disable-next-line no-extra-boolean-cast
+    if (!!validationMessage) {
+      setValidationMessage(target.validationMessage)
+    }
+  }
+
   return (
     <div>
       <textarea
@@ -20,12 +36,11 @@ const Textarea: React.FC<TextareaProps> = ({
         className={classNames(
           'block w-full rounded-md py-1 px-2 shadow-sm',
           'border border-gray-300',
-          'font-roboto text-sm font-light text-gray-900',
+          'font-roboto font-light text-gray-900 sm:text-sm',
           'focus:outline-none',
           'disabled:cursor-not-allowed disabled:opacity-75',
-          error
-            ? 'border-red-700 text-red-700 focus:ring-1 focus:ring-red-700'
-            : 'focus:border-sky-600 focus:ring-1 focus:ring-sky-600',
+          'invalid:border-red-700 invalid:text-red-700 invalid:focus:ring-1 invalid:focus:ring-red-700',
+          'focus:border-sky-600 focus:ring-1 focus:ring-sky-600',
         )}
         rows={rows}
         wrap='hard'
@@ -33,10 +48,12 @@ const Textarea: React.FC<TextareaProps> = ({
         required={required}
         name={name}
         placeholder={placeholder}
+        onBlur={onBlur}
         onChange={onChange}
+        onInvalid={onInvalid}
         {...props}
       ></textarea>
-      {error && <span className='ml-2 text-xs text-red-500'>{error}</span>}
+      {!!validationMessage && <span className='ml-2 text-xs text-red-500'>{error}</span>}
     </div>
   )
 }
